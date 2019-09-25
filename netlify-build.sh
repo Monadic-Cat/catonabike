9a377b26015a442b9948eba13e2eef753662d8bc
Monadic Cat <greenmonadcat@gmail.com>
2019-09-25 08:29:01 -0500

    Further improve build script

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/kaushalmodi/ox-hugo.git
+if [ ! -d "ox-hugo" ] ; then
+    git clone https://github.com/kaushalmodi/ox-hugo.git
+fi
 if [ ! -d "org-9.2.6" ] ; then
     curl https://orgmode.org/org-9.2.6.tar.gz --output org.tar.gz
     tar xf org.tar.gz
